2685bbdb305b13baa00518d2b976ae78b47ef4ed
Alexandre Vital <alexandrevital2005@gmail.com>
2026-03-31 16:02:13 +0100

    Refactor read_noncanonical.c for improved readability

--- TP5/read_noncanonical.c
+++ TP5/read_noncanonical.c
@@ -33,12 +33,26 @@ stateNames currentState=START;
 
 #define BUF_SIZE 9
 
+#define C_SET   0x03
+#define C_UA    0x07
+#define C_DISC  0x0B
+#define C_I0    0x00
+#define C_I1    0x40
+
 volatile int STOP = FALSE;
+
+unsigned char rr_for(int nr) {
+    return (nr == 0) ? 0x05 : 0x85;
+}
+
+unsigned char rej_for(int nr) {
+    return (nr == 0) ? 0x01 : 0x81;
+}
 
 int main(int argc, char *argv[])
 {
-    unsigned char Aread,Cread,Cant;
-    unsigned char F=0x7E,A=0x03,C=0x03,BCC1=A^C,BCC2;
+    unsigned char Aread=0,Cread=0;
+    unsigned char F=0x7E,A=0x03,BCC2_teste=0,buf=0;
 
     // Program usage: Uses either COM1 or COM2
     const char *serialPortName = argv[1];
@@ -103,20 +117,23 @@ int main(int argc, char *argv[])
 
     printf("New termios structure set\n");
 
-    unsigned char buf=0;
     unsigned char data[BUF_SIZE];
+    unsigned char reply[5];
 
-int bytes_read;
-int i=0;
+    int bytes_read;
+    int i=0;
+    int NS_esperado = 0;
 
 while(currentState!=STOPA){
 
     switch(currentState){
 
         case START:
-            Cant=Cread;
+
             printf("Current State: START\n");
+            i = 0;
             bytes_read = read(fd, &buf, 1);
+
             if (bytes_read==1 && buf==F) {
                 currentState=FLAG;
                 printf("FLAG = 0x%02X\n", buf); 
@@ -125,56 +142,65 @@ while(currentState!=STOPA){
         break;
 
         case FLAG:
+
             printf("Current State: FLAG\n");
             bytes_read = read(fd, &buf, 1);
-            if (bytes_read==1){
-                Aread=buf;
-                currentState=A_READ;
-                printf("A = 0x%02X\n", Aread); 
+
+            if (bytes_read == 1 && buf == F) {
+                currentState = FLAG;
             }
 
-
-            else if (bytes_read==1 && buf!=A && buf!=F){
-                currentState=START;
+            else if (bytes_read == 1 && buf == A) {
+                Aread = buf;
+                currentState = A_READ;
+                printf("A = 0x%02X\n", Aread);
             }
 
+            else if (bytes_read == 1) {
+                currentState = START;
+            }
         break;
 
         case A_READ:
+
             printf("Current State: A_READ\n");
             bytes_read = read(fd, &buf, 1);
-            if (bytes_read==1){
-                Cread=buf;
-                currentState=C_READ;
-                
-                printf("C = 0x%02X\n", Cread); 
+
+            if (bytes_read == 1) {
+                printf("Byte lido em A_READ = 0x%02X\n", buf);
             }
 
-            else if (bytes_read==1 && buf==F){
-                currentState=FLAG;
-
+            if (bytes_read == 1 && buf == F) {
+                currentState = FLAG;
             }
 
-            else if (bytes_read==1 && buf!=C && buf!=F){
-                currentState=START;
+            else if (bytes_read == 1 && (buf == C_I0 || buf == C_I1 || buf == C_SET || buf == C_DISC || buf == C_UA)) {
+                Cread = buf;
+                currentState = C_READ;
+                printf("C = 0x%02X\n", Cread);
+            }
+
+            else if (bytes_read == 1) {
+                currentState = START;
             }
 
         break;
 
         case C_READ:
+
             printf("Current State: C_READ\n");
             bytes_read = read(fd,&buf,1);
 
-            if (bytes_read==1 && buf==(Aread^Cread)){
+            if (bytes_read == 1 && buf == (Aread^Cread)){
                 currentState=DATA_READ;
                 printf("BCC = 0x%02X\n", buf); 
             }
 
-            else if (bytes_read==1 && buf==F){
+            else if (bytes_read == 1 && buf == F){
                 currentState=FLAG;
             }       
 
-            else if (bytes_read==1 && buf!=(Aread^Cread) && buf!=F){
+            else if (bytes_read == 1){
                 currentState=START;
             }
 
@@ -182,46 +208,127 @@ while(currentState!=STOPA){
 
 
         case DATA_READ:
-            bytes_read = read(fd,&buf,1);
-            if (bytes_read == 1 && buf==F){
-                currentState=STOPA;
-                i--;
-            }
-            
-            else {
-                data[i++]=buf;
+
+            bytes_read = read(fd, &buf, 1);
+
+            if (bytes_read == 1 && buf == F) {
+                // End of frame reached
+                if (i < 1) {
+                    // no BCC2/data
+                    printf("Frame too short\n");
+                    currentState = START;
+                    i = 0;
+                    break;
+                }
+
+                int NS_recebido = (Cread == 0x40) ? 1 : 0;
+                int tamanhoData = i - 1; // last byte stored is BCC2
+                unsigned char BCC2_recebido = data[i - 1];
+
+                printf("NS recebido = %d, expectedNs = %d\n", NS_recebido, NS_esperado);
+
+                // Prepare reply frame skeleton: F A C BCC1 F
+                reply[0] = F;
+                reply[1] = A;   // 0x03 for receiver reply
+                reply[4] = F;
+
+                if (tamanhoData < 0) {
+                    currentState = START;
+                    i = 0;
+                    break;
+                }
+
+                // If there is payload, compute BCC2
+                if (tamanhoData > 0) {
+                    BCC2_teste = data[0];
+                    for (int j = 1; j < tamanhoData; j++) {
+                        BCC2_teste ^= data[j];
+                    }
+
+                } else {
+                    // edge case: empty payload
+                    BCC2_teste = 0x00;
+                }
+
+                    printf("BCC2 calculado = 0x%02X\n", BCC2_teste);
+                    printf("BCC2 recebido  = 0x%02X\n", BCC2_recebido);
+
+                if (NS_recebido == NS_esperado) {
+                    // New frame
+                    if (BCC2_teste == BCC2_recebido) {
+                        // New and correct -> accept, send RR(nextNs)
+                        printf("New correct frame received\n");
+
+                        // Here you would pass data[0..data_len-1] to the application
+                        printf("Payload length = %d\n", tamanhoData);
+                        printf("Payload bytes: ");
+
+                        for (int j = 0; j < tamanhoData; j++) {
+                            printf("0x%02X ", data[j]);
+                        }
+
+                        printf("\n");
+
+                        NS_esperado = 1 - NS_esperado;
+
+                        reply[2] = rr_for(NS_esperado);
+                        reply[3] = reply[1] ^ reply[2];
+
+                        write(fd, reply, 5);
+
+                        // For testing a single frame, stop here.
+                        // If you want continuous reception, change to START.
+                        currentState = STOPA;
+                        }
+
+                    else {
+                        // New but with BCC2 error -> REJ(expectedNs)
+                        printf("New frame with BCC2 error\n");
+
+                        reply[2] = rej_for(NS_esperado);
+                        reply[3] = reply[1] ^ reply[2];
+
+                        write(fd, reply, 5);
+
+                        currentState = START;
+                    }
+                }
+
+                else {
+                    // Duplicate frame -> discard and send RR(expectedNs)
+                    printf("Duplicate frame received\n");
+
+                    reply[2] = rr_for(NS_esperado);
+                    reply[3] = reply[1] ^ reply[2];
+
+                    write(fd, reply, 5);
+
+                    currentState = START;
+                    }
+
+                i = 0;
             }
 
+            else if (bytes_read == 1) {
+                if (i < BUF_SIZE) {
+                     data[i++] = buf;
+                }
+
+                else {
+                    // overflow
+                    printf("Buffer overflow\n");
+                    i = 0;
+                    currentState = START;
+                    }
+                }
         break;
 
       }
 
     }
-    printf("%d\n",i);
-    printf("%d\n",data[0]);
-    unsigned char BCC2_TEST=data[0];
 
+    
 
-    for (int j=1;j<i;j++){
-        printf("%d\n",data[j]);
-        BCC2_TEST ^= data[j]+1;
-    }
-
-    printf("%d\n",BCC2_TEST);
-    printf("%d\n",data[i]);
-
-    if (BCC2_TEST != data[i]){
-        if (Cant != Cread){
-            C = 0x01;
-        }
-
-        else if (Cant == Cread){
-            C = 0x05;
-        }
-    }
-
-    unsigned char buff[5] = {F,A,C,BCC1,F};
-    write(fd, buff, 5);
 
 
   sleep(1);
